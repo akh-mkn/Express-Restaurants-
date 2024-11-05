6cd27860515261960acef8608f2cce1a7dad279d
akh-mkn <mattikn01@gmail.com>
2024-11-05 15:53:08 +0000

    update

--- src/app.js
+++ src/app.js
@@ -3,9 +3,19 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
+// PART 1!
+//app.get("/restaurants", async (req,res) =>{
+  //  const restaurants = await Restaurant.findAll()
+    //res.json(restaurants)
+//})
 
 
+//PART 2!
+app.get('/restaurants/:id', async (req, res) => {
+    const id = req.params.id;
+    const restaurant = await Restaurant.findByPk(id);
+    res.json(restaurant);
+});
 
 
 module.exports = app;
